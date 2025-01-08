0456ef89ff127d78262aca38f19f1156f4a44c78
CyberPunk <pavelrip@mail.ru>
2025-01-08 08:55:14 +0300

    period added

--- main.py
+++ main.py
@@ -14,12 +14,23 @@ blank_file_path = os.path.join(os.getcwd(), blank_file)
 
 # Функция для определения адреса ячейки по индексу буквы
 def get_cell_address(base_col, row, index):
-    col_index = ord(base_col) - ord('A') + 1 + index  # Преобразование в числовое значение столбца
-    col_letter = ''
-    while col_index > 0:
-        col_index, remainder = divmod(col_index - 1, 26)
-        col_letter = chr(65 + remainder) + col_letter
+    col_index = openpyxl.utils.column_index_from_string(base_col) + index
+    col_letter = openpyxl.utils.get_column_letter(col_index)
     return f"{col_letter}{row}"
+
+# Функция для записи значения в ячейку, учитывая объединённые ячейки
+def write_to_cell_safe(ws, cell_address, value):
+    cell = ws[cell_address]
+    if isinstance(cell, openpyxl.cell.cell.MergedCell):
+        # Получаем диапазон объединения, к которому принадлежит ячейка
+        for merge_range in ws.merged_cells.ranges:
+            if cell_address in merge_range:
+                # Записываем значение в верхнюю левую ячейку диапазона
+                top_left_cell = merge_range.min_row, merge_range.min_col
+                ws.cell(row=top_left_cell[0], column=top_left_cell[1]).value = value
+                return
+    else:
+        cell.value = value
 
 # Подготавливаем список для хранения имён новых файлов
 new_files = []
@@ -46,12 +57,21 @@ for row in patients_ws.iter_rows(min_row=2, values_only=True):
         def write_to_cells(base_col, row, text):
             for index, char in enumerate(text):
                 cell_address = get_cell_address(base_col, row, index * 2)  # Смещение через одну ячейку
-                new_ws[cell_address].value = char
+                write_to_cell_safe(new_ws, cell_address, char)
 
         # Заполняем фамилию, имя и отчество
         write_to_cells('I', 24, surname)
         write_to_cells('I', 26, name)
         write_to_cells('I', 28, patronymic)
+
+        # Заполняем период
+        def write_period(base_col, row, period_value):
+            period_str = str(period_value)  # Преобразуем численное значение в строку
+            for index, char in enumerate(period_str):
+                cell_address = get_cell_address(base_col, row, index * 2)
+                write_to_cell_safe(new_ws, cell_address, char)
+
+        write_period('BU', 11, period)
 
         # Сохраняем изменения в файле
         new_wb.save(new_file_path)
